24770fa1441ed117ac4bc7db53838b0f43337d71
LaxmiNivas <nivas.vooturi@gmail.com>
2025-03-14 21:47:25 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.logging.Logger;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> str = new ArrayList<>();
+        str.add("hai");
+        str.add("hello");
+        str.add("welcome");
+        str.add("world");
+        List<String> str1= str.subList(0,2);
+//        String[] newValueArray = str.subList(0,2).toArray(new String[0]);
+        String newValuesJoined = String.join(",", str1);
+
+        System.out.println("value "+ newValuesJoined);
+    }
+}
