c65c1ad9ea8b4773593cea348b1af7e8fd542642
LaxmiNivas <nivas.vooturi@gmail.com>
2025-03-15 11:14:40 +0530

    removed all outputs

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,11 @@ public class Main {
     public static void main(String[] args) {
 
         List<String> str = new ArrayList<>();
-        str.add("hai");
-        str.add("hello");
-        str.add("welcome");
-        str.add("world");
-         str.add("world");
+
         List<String> str1= str.subList(0,2);
 //        String[] newValueArray = str.subList(0,2).toArray(new String[0]);
         String newValuesJoined = String.join(",", str1);
 
         System.out.println("value "+ newValuesJoined);
-        str.add("world");stash 2
-        str.add("world");commit 2
-        str.add("world");commit 3
     }
 }
